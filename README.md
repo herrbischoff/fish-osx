e137a7b5e5b70aea962d16a446dd82bf005d75f1
Marcel Bischoff <marcel@herrbischoff.com>
2016-03-24 14:10:27 +0100

    Add update function

--- README.md
+++ README.md
@@ -3,6 +3,24 @@ Helpful OS X functions for fish.
 
 ## Install with Fisherman
 
+**IMPORTANT: This repository is in flux and history may be rewritten, variables and function names changed until a stable version is released. This WILL break things, so for now only install if you absolutely know what you are doing and can deal with the resulting insanity. DON'T rely on it for any kind of production workflow.**
+
 ```bash
 fisher i herrbischoff/fish-osx
 ```
+
+## Usage
+
+### update
+
+> Get OS X Software Updates, update installed Ruby gems, Homebrew, npm, pip and their installed packages.
+
+Set the variable `$fish_osx` to include the updaters you want:
+
+```fish
+set -x fish_osx asu brew npm pip ruby
+```
+
+## License
+
+See [LICENSE](LICENSE).
---
+++ functions/update.fish
@@ -0,0 +1,34 @@
+# Get OS X Software Updates, update installed Ruby gems, Homebrew, npm, pip and their installed packages
+function update
+    switch (uname)
+    case Darwin
+        if contains asu $fish_osx
+            echo "=====[ Apple Software Update ]===================================="; and \
+            sudo softwareupdate -i -a
+        end
+        if contains brew $fish_osx
+            echo "=====[ Homebrew ]================================================="; and \
+            brew update; and \
+            brew upgrade; and \
+            brew cleanup -s; and \
+            brew cask cleanup
+        end
+        if contains npm $fish_osx
+            echo "=====[ npm ]======================================================"; and \
+            npm install npm -g; and \
+            npm update -g
+        end
+        if contains ruby $fish_osx
+            echo "=====[ Gems ]====================================================="; and \
+            sudo gem update --system; and \
+            sudo gem update
+        end
+        if contains pip $fish_osx
+            echo "=====[ pip ]======================================================"; and \
+            sudo python -m pip install --upgrade (python -m pip list --outdated | awk '/.*/ {print $1}'); and \
+            sudo python3 -m pip install --upgrade (python3 -m pip list --outdated | awk '/.*/ {print $1}')
+        end
+    case '*'
+        echo Command not supported on (uname).
+    end
+end
